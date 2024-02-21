e1d9e37ca1b11fb61627468dec2e2ff9a54ad1db
CarlTheOdd <160284622+CarlTheOdd@users.noreply.github.com>
2024-02-21 17:57:04 -0500

    Add files via upload

---
+++ saves.js
@@ -0,0 +1,35 @@
+var state = {
+    quark,
+    accelCost,
+    accelIncrease,
+    accelLevel,
+    nucleusCost,
+    nucleusIncrease,
+    nucleusLevel,
+    atomCost,
+    atomIncrease,
+    atomLevel,
+    moleCost,
+    moleIncrease,
+    moleLevel,
+    qpc,
+    qps,
+    aps,
+    npfs,
+    mole_control,
+    show_mole_poss,
+    prestigeCost,
+    prestigeAmount,
+    multiplier
+}
+
+var SAVE_KEY = 'save';
+
+function save(){
+    localStorage.setItem(SAVE_KEY, JSON.stringify(state));
+}
+
+function load(){
+    var load_state = JSON.parse(localStorage.getItem(SAVE_KEY));
+    state = load_state;
+}
