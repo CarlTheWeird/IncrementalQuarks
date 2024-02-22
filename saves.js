70604f7afbab00a16d9fc4e582ab112820729c8d
CarlTheOdd <160284622+CarlTheOdd@users.noreply.github.com>
2024-02-22 08:39:46 -0500

    Update saves.js

--- saves.js
+++ saves.js
@@ -32,27 +32,12 @@ var SAVE_KEY = 'save';
 function save(){
     window.localStorage.setItem(SAVE_KEY, JSON.stringify(state));
     alert("Saved!");
-    alert(state.quark);
 }
 
 function load(){
     var load_state = JSON.parse(window.localStorage.getItem(SAVE_KEY));
     
     for(i = -1; i < load_state.length; i++){
-        state[i] = load_state[i];
+        state = load_state;
     }
-
-    initHTML();
-}
-
-function initHTML(){
-    quark.innerHTML = quantify(parsedQuark);
-    accelCost.innerHTML = quantify(parsedAccelCost);
-    accelIncrease.innerHTML = quantify(parsedAccelIncrease);
-    nucleusCost.innerHTML = quantify(parsedNucleusCost);
-    nucleusIncrease.innerHTML = quantify(parsedNucleusIncrease);
-    atomCost.innerHTML = quantify(parsedAtomCost);
-    atomIncrease.innerHTML = quantify(parsedAtomIncrease);
-    moleCost.innerHTML = quantify(parsedMoleCost);
-    moleIncrease.innerHTML = quantify(parsedMoleIncrease);
 }
