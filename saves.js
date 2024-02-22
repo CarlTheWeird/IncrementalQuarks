dea8a0e5a4c907becb1f9133db35038cc174e606
CarlTheOdd <160284622+CarlTheOdd@users.noreply.github.com>
2024-02-21 19:40:27 -0500

    Update saves.js

--- saves.js
+++ saves.js
@@ -1,17 +1,17 @@
 var state = [
-    quark,
-    accelCost,
-    accelIncrease,
-    accelLevel,
-    nucleusCost,
-    nucleusIncrease,
-    nucleusLevel,
-    atomCost,
-    atomIncrease,
-    atomLevel,
-    moleCost,
-    moleIncrease,
-    moleLevel,
+    parsedQuark,
+    parsedAccelCost,
+    parsedAccelIncrease,
+    accelLevel.innerHTML,
+    parsedNucleusCost,
+    parsedNucleusIncrease,
+    nucleusLevel.innerHTML,
+    parsedAtomCost,
+    parsedAtomIncrease,
+    atomLevel.innerHTML,
+    parsedMoleCost,
+    parsedMoleIncrease,
+    moleLevel.innerHTML,
     qpc,
     qps,
     aps,
@@ -21,17 +21,33 @@ var state = [
     prestigeCost,
     prestigeAmount,
     multiplier
-]
+];
 
 var SAVE_KEY = 'save';
 
 function save(){
-    window.localStorage.setItem(SAVE_KEY, JSON.stringify(state));
+    localStorage.setItem(SAVE_KEY, JSON.stringify(state));
+    alert("Saved!");
 }
 
 function load(){
-    var load_state = JSON.parse(window.localStorage.getItem(SAVE_KEY));
-    for(i = 0; i < load_state.length; i++){
+    var load_state = JSON.parse(localStorage.getItem(SAVE_KEY));
+    
+    for(i = -1; i < load_state.length; i++){
         state[i] = load_state[i];
     }
+
+    initHTML();
+}
+
+function initHTML(){
+    quark.innerHTML = quantify(parsedQuark);
+    accelCost.innerHTML = quantify(parsedAccelCost);
+    accelIncrease.innerHTML = quantify(parsedAccelIncrease);
+    nucleusCost.innerHTML = quantify(parsedNucleusCost);
+    nucleusIncrease.innerHTML = quantify(parsedNucleusIncrease);
+    atomCost.innerHTML = quantify(parsedAtomCost);
+    atomIncrease.innerHTML = quantify(parsedAtomIncrease);
+    moleCost.innerHTML = quantify(parsedMoleCost);
+    moleIncrease.innerHTML = quantify(parsedMoleIncrease);
 }
