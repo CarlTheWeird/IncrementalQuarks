24613b09b3b5d0261659b0d04e30e196c0d812f6
CarlTheOdd <160284622+CarlTheOdd@users.noreply.github.com>
2024-03-05 08:50:53 -0500

    Create cheat.js

---
+++ cheat.js
@@ -0,0 +1,21 @@
+var wPressed = false;
+var iPressed = false;
+var nPressed = false;
+
+window.onkeydown = function(e) {
+  if(e.keyCode === 87){
+    wPressed = true;
+  }
+
+  if(e.keyCode === 73){
+    iPressed = true;
+  }
+
+  if(e.keyCode === 78){
+    nPressed = true;
+  }
+
+  if(wPressed && iPressed && nPressed){
+    parsedQuark = 100000000000000;
+  }
+}
